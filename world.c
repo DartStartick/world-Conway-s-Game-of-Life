d1fe1daef16a88c3a8346ba7e9aa774b8dc0135e
DartStartick <69866726+DartStartick@users.noreply.github.com>
2023-05-19 12:33:53 +0200

    world.c

--- world.c
+++ world.c
@@ -1,198 +1,171 @@
-#include "world.h"
-#include <assert.h>
 #include <stdio.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <curses.h>
 
-int TIMEOUT;
+#include "world.h"
 
-void abort_game(const char* message){
-    endwin();
-    puts(message);
-    exit(1);
+#define MOORE_SEARCH_DEPTH 1
+
+#define WORLD_WIDTH 230
+#define WORLD_HEIGHT 50
+
+#define LOOP_ADVANCE_DELAY 100
+
+bool* newDeadCells(size_t width, size_t height) {
+    // Создание нового массива клеток, изначально все мертвы (Vytvorenie nového poľa buniek, ktoré sú pôvodne všetky mŕtve)
+    bool* cells = malloc(sizeof(bool) * width * height);
+
+    for (size_t i = 0; i < (width * height); i++) {
+        cells[i] = false;
+    }
+
+    return cells;
 }
 
-void check_bounds(const char* source,int x, int y){
-    char msg[200];
-    if (x < 0 || x >= COLS){
-        sprintf(msg,"%s:: width %d is out of bounds (0,%d)",source,x,COLS);
-        abort_game(msg);
-    }
-    if (y < 0 || y >= LINES){
-        sprintf(msg,"%s:: height %d is out of bounds (0,%d)",source,y,LINES);
-        abort_game(msg);
-    }
+World* newWorld(size_t width, size_t height) {
+    // Создание нового мира заданных размеров (Vytvorenie nového sveta definovaných rozmerov)
+    World* w = malloc(sizeof(World));
+
+    w->width = width;
+    w->height = height;
+
+    // Создание массивов для чтения и записи клеток (Vytvorenie polí na čítanie a zápis buniek)
+    w->readCells = newDeadCells(width, height);
+    w->writeCells = newDeadCells(width, height);
+
+    return w;
 }
 
-void clear_screen(){
-    // Clear screen
-    mvaddch(0,0,' ');
-    int screenchars = LINES*COLS;
-    for (int j = 1; j < screenchars;j++ ){
-        addch(' ');
-    }
-}
+void randomizeCells(World* world, float aliveChance) {
+    // Случайная инициализация клеток с заданной вероятностью жизни (Náhodná inicializácia buniek s danou pravdepodobnosťou života)
+    size_t width = world->width;
+    size_t height = world->height;
 
-void game_speed(int value){
-    if (value < 0){
-        abort_game("world_seed:: cannot be negative\n");
-    }
-    TIMEOUT =value;
-}
-
-void set_message(const char* message,int x,int y) {
-    int l = strlen(message);
-    for (int i = 0; i < l; i++){
-        check_bounds("set_message",x+i,y);
-        set_cell(message[i],x+i,y);
-    }
-}
-
-void assert_message(int event,const char* message){
-    if (event == 0){
-        abort_game(message);
+    for (size_t i = 0; i < (width * height); i++) {
+        world->readCells[i] = (rand() % 100) < aliveChance * 100;
     }
 }
 
-
-void set_cell(int character,int x,int y) {
-    check_bounds("set_cell",x,y);
-    set_color_cell(character,x,y,COLOR_WHITE,COLOR_BLACK);
+void copyWritableToReadable(World* world) {
+    // Копирование клеток из массива записи в массив чтения (Kopírovanie buniek z poľa na zápis do poľa na čítanie)
+    memcpy(world->readCells, world->writeCells,
+           sizeof(bool) * world->width * world->height);
 }
 
-void set_color_cell(int character,int x,int y,short front_color,short back_color){
-    check_bounds("set_color_cell",x,y);
-    if (has_colors()){
-        int pair = COLOR_COUNT * front_color + back_color; 
-        attron(COLOR_PAIR(pair));
-        mvaddch(y,x,character);
-        attroff(COLOR_PAIR(pair));
-    }
-    else{
-        mvaddch(y,x,character);
-    }
+void freeWorld(World* world) {
+    // Освобождение памяти, занятой миром и его клетками (Uvoľnenie pamäte obsadenej svetom a jeho bunkami)
+    free(world->readCells);
+    free(world->writeCells);
+    free(world);
 }
 
-int start_world(void* (*init_game)(),int (*world_event)(struct event* event,void* game),void (*destroy_game)(void*)){
-    srand(time(NULL));
-    int r = 1;
-    // Speed global variable
-    TIMEOUT = 100;
-    if (initscr() == NULL){
-        // TODO Which Error?
-        puts("Curses Error.");
-        return -1;
-    }
-    noecho(); // Nevypisuj vstup na obrazovku
-    cbreak(); // Zabudni starý vstup
-    nodelay(stdscr,TRUE); // Nečakaj na stlačenie
-    keypad(stdscr,TRUE); // Aktivuje šípky
-    curs_set(FALSE); // Neviditeľný kurzor
-    /* Get all the mouse events */
-    mousemask(ALL_MOUSE_EVENTS, NULL);
-    MEVENT mouse_event;
-    if (has_colors()){ // Zistenie či terminál podporuje farby
-        start_color();
-        for (int i = 0; i < COLOR_COUNT;i++){
-            for (int j = 0; j < COLOR_COUNT;j++){
-                init_pair(i * COLOR_COUNT + j, i,j);
+size_t indexOf(size_t width, size_t x, size_t y) {
+    // Получение индекса клетки в одномерном массиве по координатам (Získanie indexu bunky v jednorozmernom poli pomocou súradníc)
+    return width * y + x;
+}
+
+void setCell(World* world, size_t x, size_t y, bool isAlive) {
+    // Установка состояния клетки по заданным координатам (Nastavenie stavu bunky podľa zadaných súradníc)
+    size_t index = indexOf(world->width, x, y);
+
+    world->writeCells[index] = isAlive;
+}
+
+bool getCell(const World* world, size_t x, size_t y) {
+    // Получение состояния клетки по заданным координатам (Získanie stavu bunky zo zadaných súradníc)
+    int index = indexOf(world->width, x, y);
+
+    return world->readCells[index];
+}
+
+size_t min(size_t a, size_t b) {
+    // Возвращает минимальное значение из двух (Vracia minimálnu hodnotu z dvoch)
+    return (a < b) ? a : b;
+}
+
+size_t max(size_t a, size_t b) {
+    // Возвращает максимальное значение из двух (Vracia maximálnu hodnotu dvoch)
+    return (a > b) ? a : b;
+}
+
+size_t nAliveNeighborsSurrounding(World* world, size_t x, size_t y, size_t depth) {
+    // Подсчет количества живых соседей в заданной области вокруг клетки (Počítanie počtu žijúcich susedov v danej oblasti okolo bunky)
+    size_t xBound = min(x + depth + 1, world->width);
+    size_t yBound = min(y + depth + 1, world->height);
+
+    size_t aliveCount = 0;
+    for (size_t ny = max(0, y - depth); ny < yBound; ny++) {
+        for (size_t nx = max(0, x - depth); nx < xBound; nx++) {
+
+            if (getCell(world, nx, ny) && !(nx == x && ny == y)) {
+                aliveCount++;
             }
         }
     }
-    else {
-        puts("No colors!\n");
-    }
-    void* game = NULL;
-    if (init_game != NULL){
-         game = init_game();
-         assert_message(game != NULL,"init_game:: should return non null pointer");
-    }
-    timeout(TIMEOUT);
-    // Initial step
-    struct event event;
-    memset(&event,0,sizeof(struct event));
-    event.height = LINES;
-    event.width = COLS;
-    event.type = EVENT_START;
-    clock_t start_time = clock();
-    clock_t last_timeout = start_time;
-    clock_t next_timeout = last_timeout + TIMEOUT;
-    event.time_ms = start_time;
-    // Start event
-    r = world_event(&event,game);
-    refresh();
-    while (!r) {
-        memset(&event,0,sizeof(struct event));
-        event.height = LINES;
-        event.width = COLS;
-        event.key = getch();
-        // No key was pressed
-        if (event.key == ERR){
-            event.type = EVENT_TIMEOUT;
-            last_timeout = clock();
-            next_timeout = last_timeout + TIMEOUT;
-        }
-        // Mouse event
-        else if (event.key == KEY_MOUSE ){
-            event.type = EVENT_MOUSE;
-            if(getmouse(&mouse_event) == OK){
-                event.mouse_x = mouse_event.x;
-                event.mouse_y = mouse_event.y;
-                if(mouse_event.bstate & BUTTON1_PRESSED){
-                    event.mouse_left = 1;
-                }
-                if(mouse_event.bstate & BUTTON2_PRESSED){
-                    event.mouse_middle = 1;
-                }
-                if(mouse_event.bstate & BUTTON3_PRESSED){
-                    event.mouse_right = 1;
-                }
-            }
-        }
-        else if (event.key == KEY_RESIZE) {
-            event.type = EVENT_RESIZE;
-        }
-        else{
-            event.type = EVENT_KEY;
-            if (event.key == 27){
-                int k = getch();
-                if (k == -1){
-                    // Esc Was pressed
-                    event.type = EVENT_ESC;
-                }
-                else {
-                    // Alt was pressed
-                    event.key = k;
-                    event.alt_key = 1;
-                }
-            }
-        }
-        // Draw new world
-        event.time_ms = clock();
-        r = world_event(&event,game);
-        refresh();
-        event.time_ms = clock();
-        // set new timeout
-        int nt = next_timeout - event.time_ms;
-        //printf("%d\n",nt);
-        if (nt > 0){
-            timeout(nt);
-        }
-        else {
-            timeout(TIMEOUT);
-            next_timeout = event.time_ms + TIMEOUT;
+
+    return aliveCount;
+}
+
+bool cellShouldLive(bool isAlive, size_t nNeighbors) {
+    // Определяет, должна ли клетка остаться живой или стать живой в следующем поколении (Určuje, či bunka zostane živá alebo sa stane živou v ďalšej generácii.)
+    return (isAlive && nNeighbors >= 2 && nNeighbors <= 3)
+        || (!isAlive && nNeighbors == 3);
+}
+
+void advanceCellAt(World* world, size_t x, size_t y) {
+    // Продвигает клетку на один шаг во времени (posunie bunku o jeden krok v čase)
+    size_t nNeighbors = nAliveNeighborsSurrounding(world, x, y, MOORE_SEARCH_DEPTH);
+    bool isAlive = getCell(world, x, y);
+
+    setCell(world, x, y, cellShouldLive(isAlive, nNeighbors));
+}
+
+void advanceWorld(World* world) {
+    // Продвигает весь мир на один шаг во времени (Posúvanie celého sveta o krok ďalej v čase)
+    size_t width = world->width;
+    size_t height = world->height;
+
+    for (size_t y = 0; y < height; y++) {
+        for (size_t x = 0; x < width; x++) {
+            advanceCellAt(world, x, y);
         }
     }
-    memset(&event,0,sizeof(struct event));
-    event.height = LINES;
-    event.width = COLS;
-    event.type = EVENT_END;
-    event.time_ms = clock();
-    world_event(&event,game);
-    if (destroy_game != NULL){
-        destroy_game(game);
+
+    copyWritableToReadable(world);
+}
+
+void formatWorld(World* world, WINDOW* win) {
+    // Форматирование и вывод состояния мира в окно curses (Formátovanie a zobrazenie stavu sveta v okne curses)
+    size_t width = world->width;
+    size_t height = world->height;
+
+    for (size_t y = 0; y < height; y++) {
+        for (size_t x = 0; x < width; x++) {
+            bool isAlive = getCell(world, x, y);
+            char rep = isAlive ? '#' : ' ';
+            mvwaddch(win, y, x, rep);
+        }
     }
-    endwin();
-    return r;
-};
+}
+
+void printWorld(World* world, WINDOW* win) {
+    // Вывод состояния мира в окно curses (Zobrazenie stavu sveta v okne curses)
+    formatWorld(world, win);
+    wrefresh(win);
+}
+
+void simpleConsoleRoutine() {
+    // Простая реализация игрового цикла для консольного вывода (Jednoduchá implementácia hernej slučky pre výstup na konzolu)
+    srand((unsigned int)time(NULL));
+
+    World* world = newWorld(WORLD_WIDTH, WORLD_HEIGHT);
+    randomizeCells(world, 0.1);
+
+    for (size_t i = 0;; i++) {
+        printWorld(world, stdscr);
+        napms(LOOP_ADVANCE_DELAY);
+    }
+} 
